3ec4f5c86743cc8bc5250b2d8b477b50e9467bc1
yirian <yirian@yirian-HP-Pavilion-dv6500-Notebook-PC>
2018-01-09 16:22:03 -0500

    Creating The Project.

---
+++ src/MyAction.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.RecursiveAction;
+
+public class MyAction extends RecursiveAction {
+
+	@Override
+	protected void compute() {
+		
+		
+	}
+
+}
---
+++ src/MyTask.java
@@ -0,0 +1,6 @@
+import java.util.concurrent.RecursiveTask;
+public class MyTask extends RecursiveTask<Integer> {
+	public Integer compute() {
+		return 0;
+	}
+}
