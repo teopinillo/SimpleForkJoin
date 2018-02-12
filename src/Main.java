178f04f59019d9f90a7f34dd16775186fd9e2aaf
yirian <yirian@yirian-HP-Pavilion-dv6500-Notebook-PC>
2018-02-12 18:05:50 -0500

    Craeting main class and RecursiveAction class.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
+import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("using recursive action to simulate");
+		Main myApp = new Main();		
+		System.out.println("using recursive action to get the square root of an array");
 		System.out.println("ants discharging a 100 pounds bag of sugar.");
-		MyAction myAction = new MyAction(100);
+		int l = 20;
+		double[] numbers = myApp.createArray(l);
+		MyAction myAction = new MyAction(numbers,0,l);
 		ForkJoinPool pool = new ForkJoinPool();
 		pool.invoke (myAction);
+	}
+	
+	public double[] createArray(int l) {
+		Random random = new Random(100);
+		double[] numbers = new double [l];
+		 for (int i=0;i<l;i++) {
+			 numbers[i]= random.nextDouble();
+			 System.out.println(numbers[i]+" ");
+		 }
+		 return numbers;
 	}
 
 }
--- src/MyAction.java
+++ src/MyAction.java
@@ -1,30 +1,31 @@
 import java.util.concurrent.RecursiveAction;
 
+@SuppressWarnings("serial")
 public class MyAction extends RecursiveAction {
 
-	private int pounds;
+	private double[] numbers;
+	private int iStart;
+	private int iEnd;
+	
 	
 	@Override
 	protected void compute() {
-		if (pounds<4) doingMySelf(pounds);
-		 else {
-			RecursiveAction left = new MyAction(pounds / 2);
-			RecursiveAction right = new MyAction(pounds /2);
-			//invokeAll(left,right);
-			left.fork();
-			right.fork();
-			left.join();
-			right.join();
-		}
-		
+		//I will do 4 elements
+		if (iEnd-iStart<=4) {
+			for (int i =iStart;i<=iEnd;i++) {
+				numbers[i]=Math.sqrt(numbers[i]);
+			}
+		} else {
+			int mid = (iEnd-iStart)/2;
+			RecursiveAction left = new MyAction (numbers,iStart,iStart+mid-1);
+			RecursiveAction rigth = new MyAction (numbers,iStart+mid,iEnd);
+			invokeAll(left,rigth);
+		}				
 	}
 	
-	public MyAction (int pounds) {
-		this.pounds = pounds;
+	public MyAction (double[] numbers, int start, int ends) {
+		this.numbers = numbers;
+		iStart = start;
+		iEnd = ends;
 	}
-	
-	public void doingMySelf(int pounds) {
-		System.out.println("I'm taking " + pounds+ "to the cave");
-	}
-
 }
