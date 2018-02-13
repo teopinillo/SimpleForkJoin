5b8c30d820f36460f15db7603a41b70cf5ca7e21
yirian <yirian@yirian-HP-Pavilion-dv6500-Notebook-PC>
2018-02-12 19:06:41 -0500

    New method to print the array.

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,29 @@ public class Main {
 	public static void main(String[] args) {
 		Main myApp = new Main();		
 		System.out.println("using recursive action to get the square root of an array");
-		System.out.println("ants discharging a 100 pounds bag of sugar.");
+		
 		int l = 20;
 		double[] numbers = myApp.createArray(l);
-		MyAction myAction = new MyAction(numbers,0,l);
+		myApp.printArray(numbers);
+		MyAction myAction = new MyAction(numbers,0,l-1);
 		ForkJoinPool pool = new ForkJoinPool();
 		pool.invoke (myAction);
+		myApp.printArray(numbers);
 	}
 	
 	public double[] createArray(int l) {
 		Random random = new Random(100);
 		double[] numbers = new double [l];
 		 for (int i=0;i<l;i++) {
-			 numbers[i]= random.nextDouble();
-			 System.out.println(numbers[i]+" ");
+			 numbers[i]= random.nextDouble()*100;			 
 		 }
 		 return numbers;
+	}
+	
+	public void printArray(double[] a) {
+		System.out.println("");
+		for (double d:a)
+			System.out.format("%2.2f ",d);
 	}
 
 }
